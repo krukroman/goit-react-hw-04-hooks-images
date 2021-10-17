b2cb6a00adc9a2a8bb9d60d3f04698ce634ab7f0
Roman Kruk <wilmlass@gmail.com>
2021-10-17 23:14:40 +0300

    rewrite Modal with useEffect hook

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import Button from '../Button';
@@ -6,48 +6,45 @@ import { ReactComponent as CloseIcon } from './icon-close.svg';
 
 const portal = document.getElementById('portal');
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onEscape);
-  }
+export default function Modal({ image, onClose }) {
+  const { url, tags } = image;
+  useEffect(() => {
+    window.addEventListener('keydown', onEscape);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onEscape);
-  }
+    return () => {
+      window.removeEventListener('keydown', onEscape);
+    };
+  });
 
-  onBackdropClick = e => {
+  const onBackdropClick = e => {
     const { currentTarget, target } = e;
     if (currentTarget === target) {
       this.props.onClose();
     }
   };
 
-  onEscape = e => {
+  const onEscape = e => {
     const { code } = e;
 
     if (code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  onCloseBtnClick = e => {
+  const onCloseBtnClick = e => {
     e.preventDefault();
-    this.props.onClose();
+    onClose();
   };
 
-  render() {
-    const { url, tags } = this.props.image;
-    const onBackdropClick = this.onBackdropClick;
-    return createPortal(
-      <div className="Overlay" onClick={onBackdropClick}>
-        <img src={url} alt={tags} />
-        <Button className="CloseModalButton" onClick={this.onCloseBtnClick}>
-          <CloseIcon />
-        </Button>
-      </div>,
-      portal,
-    );
-  }
+  return createPortal(
+    <div className="Overlay" onClick={onBackdropClick}>
+      <img src={url} alt={tags} />
+      <Button className="CloseModalButton" onClick={onCloseBtnClick}>
+        <CloseIcon />
+      </Button>
+    </div>,
+    portal,
+  );
 }
 
 Modal.propTypes = {
